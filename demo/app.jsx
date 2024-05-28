f536c195bea7a7e7d742e43bb5d9cb21654f57f6
ppzreboot <164493036+ppzreboot@users.noreply.github.com>
2024-05-28 07:08:13 +0000

    暂存老代码

--- demo/app.jsx
+++ demo/app.jsx
@@ -9,9 +9,14 @@ export
 function App() {
 	return <div>
 		<HH_ctx_provider>
-			<Canvas width={500} height={500} />
-			<Layer_mng />
 			<Obj_adder />
+			<div style={{
+				display: 'inline-block',
+				'box-shadow': '0 0 10px rgba(0,0,0, .1)',
+			}}>
+				<Canvas width={500} height={500} />
+			</div>
+			<Layer_mng />
 		</HH_ctx_provider>
 	</div>
 }
